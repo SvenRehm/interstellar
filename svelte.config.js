ba88a153f64c6d77b27cc4eddd8b07bc47ae547e
Rehm.Sven <rehm21@web.de>
2024-12-20 03:11:42 +0100

    svelte deploy config

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,23 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+// import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter({
-			precompress: false,
-			fallback: 'index.html'
-		})
-	}
-};
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+  kit: {
+    //this is from the phaser template
+    // adapter: adapter({
+    //   precompress: false,
+    //   fallback: 'index.html'
+    // })
+    adapter: adapter({
+      //vercel config
+      // see below for options that can be set here
+    })
+  }
+}
 
-export default config;
+export default config
